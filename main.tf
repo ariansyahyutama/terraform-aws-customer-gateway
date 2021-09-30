3327d7db65af92fb84df8f9b523374caf8f6d670
ariansyahyutama <67248935+ariansyahyutama@users.noreply.github.com>
2021-09-30 09:12:15 +0700

    adding variable country, city, service provder

--- main.tf
+++ main.tf
@@ -1,19 +1,33 @@
+/*common_tags = merge(
+      var.additional_tags,
+      {
+          "ProductDomain" = var.product_domain
+      },
+      {
+          "Environment" = var.environment
+      },
+      {
+          "managedBy" = "terraform"
+      }
+  )*/
 
-resource "aws_customer_gateway" "this" {
-  for_each = var.create ? var.customer_gateways : {}
-  
+locals {
   country   = var.country_id
   city      = var.city_id
   isp       = var.isp_id
   building  = var.building_id
-
+  }
+resource "aws_customer_gateway" "this" {
+  for_each = var.create ? var.customer_gateways : {}
+  
   bgp_asn    = each.value["bgp_asn"]
   ip_address = each.value["ip_address"]
   type       = "ipsec.1"
 
+
   tags = merge(
     {
-      Name = format("%s-%s-%s-%s", var.country_id, var.city_id, var.building_id, var.isp_id)
+      Name = format("%s-%s-%s-%s", local.country_id, local.city_id, local.building_id, local.isp_id)
     },
     var.tags
   )
