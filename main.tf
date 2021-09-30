5ca9a9b75a97534de4f597bdb351686a876e2bf0
ariansyahyutama <67248935+ariansyahyutama@users.noreply.github.com>
2021-09-30 09:32:18 +0700

    adding variable country, city, service provder

--- main.tf
+++ main.tf
@@ -11,19 +11,14 @@
       }
   )*/
 
-/*locals {
-  country   = var.country_id
-  city      = var.city_id
-  isp       = var.isp_id
-  building  = var.building_id
-  }*/
+
 resource "aws_customer_gateway" "this" {
   for_each = var.create ? var.customer_gateways : {}
   
-  #country   = var.country_id
-  #city      = var.city_id
-  #isp       = var.isp_id
-  #building  = var.building_id
+  country   = each.value["var.country_id"]
+  city      = each.value["var.city_id"]
+  isp       = each.value["var.isp_id"]
+  building  = each.value["var.building_id"]
 
   bgp_asn    = each.value["bgp_asn"]
   ip_address = each.value["ip_address"]
@@ -32,7 +27,7 @@ resource "aws_customer_gateway" "this" {
 
   tags = merge(
     {
-      Name = format("%s-%s-%s-%s", var.country_id, var.city_id, var.building_id, var.isp_id)
+      Name = format("%s-%s-%s-%s", country, city, building, isp)
     },
     var.tags
   )
