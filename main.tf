6caaa2aec614a960fe59eac8327f27f8eced6127
Ariansyah Yutama <67248935+ariansyahyutama@users.noreply.github.com>
2025-03-19 16:06:48 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
+# Resource definition
 resource "aws_customer_gateway" "this" {
   for_each = var.create ? var.customer_gateways : {}
 
   bgp_asn    = each.value["bgp_asn"]
   ip_address = each.value["ip_address"]
   type       = "ipsec.1"
-
 
   tags = merge(
     {
@@ -13,3 +13,24 @@ resource "aws_customer_gateway" "this" {
     var.tags
   )
 }
+
+# Output definition (should be outside of resource blocks)
+output "gateway_details" {
+  description = "Map of customer gateway IDs and IP addresses by key"
+  value = {
+    for k, v in aws_customer_gateway.this : k => {
+      id = aws_customer_gateway.this[k].id
+      ip_address = aws_customer_gateway.this[k].ip_address
+    }
+  }
+}
+
+# You should also keep your existing outputs if needed, or update them
+output "ids" {
+  value = [for cgw in aws_customer_gateway.this : cgw.id]
+}
+
+output "ip_add" {
+  value = [for cgw in aws_customer_gateway.this : cgw.ip_address]
+}
+
